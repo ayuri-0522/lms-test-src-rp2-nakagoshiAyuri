c9b986abb8344885ad46a3eae7d6a8d0f96d5c2d
nakagoshi.ayuri <nakagoshi.ayuri@192.168.0.8>
2026-09-18 17:45:22 +0900

    add:Case03.java
    ログイン画面を表示しログイン後コース詳細画面に遷移するかの確認のテストを作成した
    期待値は以下の通り
    パンくずリスト:コース詳細 | LMS
    URL："http://localhost:8080/lms/course/detail"

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,11 +10,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
  * ケース03
- * @author holy
+ * @author 中越愛百合
  */
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース03 受講生 ログイン 正常系")
@@ -35,14 +37,41 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//指定したURLの画面を開く
+		webDriver.get("http://localhost:8080/lms");
+
+		//指定したURLと一致しているか確認する
+		assertEquals("http://localhost:8080/lms/", webDriver.getCurrentUrl());
+
+		//test1のエビデンスを取得する
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// 1. ログイン画面を開く
+		webDriver.get("http://localhost:8080/lms");
+
+		// 2. ログイン・パスワードを入力
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA1");
+
+		// 3. ログインボタンをクリック
+		webDriver.findElement(By.className("btn-primary")).click();
+
+		// 4. コース詳細画面のURLになっているか確認する
+		assertEquals("http://localhost:8080/lms/course/detail", webDriver.getCurrentUrl());
+
+		// 5. コース詳細画面が遷移した（タイトルが変わったか）ことの確認
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		//test3のエビデンスを取得する
+		getEvidence(new Object() {
+		});
 	}
 
 }
