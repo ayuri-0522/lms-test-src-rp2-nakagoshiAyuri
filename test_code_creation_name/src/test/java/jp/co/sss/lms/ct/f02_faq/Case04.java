1dc072c67f4e623d49953bf21a3508d051bb52b2
nakagoshi.ayuri <nakagoshi.ayuri@192.168.0.8>
2026-09-24 11:40:56 +0900

    add:Case04.java よくある質問への遷移の確認
    上部メニューの「ヘルプ」「よくある質問」リンクから各画面へ遷移し、
    遷移先のパンくずリストとURLが期待値どおりであることを確認するテストを追加
    期待値は以下の通り
    パンくずリスト：よくある質問 | LMS
    URL: http://localhost:8080/lms/faq

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト よくある質問機能
@@ -31,32 +37,113 @@ public class Case04 {
 		closeDriver();
 	}
 
+	//1.待ち時間を最大10秒に設定する
+	final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//指定したURLの画面を開く
+		webDriver.get("http://localhost:8080/lms");
+
+		//指定したURLと一致しているか確認する
+		assertEquals("http://localhost:8080/lms/", webDriver.getCurrentUrl());
+
+		//test1のエビデンスを取得する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		// 1. ログイン・パスワードを入力
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA1");
+
+		// 2. ログインボタンをクリック
+		webDriver.findElement(By.className("btn-primary")).click();
+
+		// 3. コース詳細画面のURLが表示されるまで待つ
+		wait.until(ExpectedConditions.urlToBe("http://localhost:8080/lms/course/detail"));
+
+		// 4. コース詳細画面のURLになっているか確認する
+		assertEquals("http://localhost:8080/lms/course/detail", webDriver.getCurrentUrl());
+
+		// 5. コース詳細画面が遷移した（タイトルが変わったか）ことの確認
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		//6.test2のエビデンスを取得する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		// 1. ヘッダーの機能リンクをクリック
+		webDriver.findElement(By.className("dropdown-toggle")).click();
+
+		// 2. ヘッダーの機能タグのヘルプリンクをクリック
+		wait.until(ExpectedConditions.elementToBeClickable(By.linkText("ヘルプ")));
+
+		// 3. ヘッダーの機能リンクをクリック
+		webDriver.findElement(By.linkText("ヘルプ")).click();
+
+		// 4. コース詳細画面のURLが表示されるまで待つ
+		wait.until(ExpectedConditions.urlToBe("http://localhost:8080/lms/help"));
+
+		// 5. コース詳細画面のURLになっているか確認する
+		assertEquals("http://localhost:8080/lms/help", webDriver.getCurrentUrl());
+
+		// 6. コース詳細画面が遷移した（タイトルが変わったか）ことの確認
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());
+
+		// 7.test3のエビデンスを取得する
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		// 1.元のタブを取得
+		String originalWindow = webDriver.getWindowHandle();
+
+		// 2.ヘッダーの機能タグのヘルプリンクをクリック
+		wait.until(ExpectedConditions.elementToBeClickable(By.linkText("よくある質問")));
+
+		// 3.ヘッダーの機能リンクをクリック
+		webDriver.findElement(By.linkText("よくある質問")).click();
+
+		// 4.新しいタブが開くまで待つ
+		wait.until(
+				ExpectedConditions.numberOfWindowsToBe(2));
+
+		// 5.新しいタブへ切り替える
+		for (String windowHandle : webDriver.getWindowHandles()) {
+			if (!windowHandle.equals(originalWindow)) {
+				webDriver.switchTo().window(windowHandle);
+				break;
+			}
+		}
+
+		// 6. コース詳細画面のURLになっているか確認する
+		assertEquals("http://localhost:8080/lms/faq", webDriver.getCurrentUrl());
+
+		// 7. コース詳細画面が遷移した（タイトルが変わったか）ことの確認
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());
+
+		// 8.test4のエビデンスを取得する
+		getEvidence(new Object() {
+		});
 	}
 
 }
